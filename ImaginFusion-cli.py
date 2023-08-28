67547d9be22550062ad719db774420d331226472
imyaash <yashppanchal1997@gmail.com>
2023-08-28 14:44:42 +0100

    minor updates

--- ImaginFusion-cli.py
+++ ImaginFusion-cli.py
@@ -6,6 +6,8 @@ def main():
     parser = argparse.ArgumentParser(description = "ImaginFusionCLI")
     parser.add_argument("posPrompt", type = str, help = "Positive prompt for ImaginFusion")
     parser.add_argument("workspace", type = str, help = "Workspace for saving results")
+    parser.add_argument("--sdVersion", type = str, default = "2.1", help = "Stable Diffusion version")
+    parser.add_argument("--hfModelKey", type = str, default = None, help = "Hugging Face model key for Stable Diffusion")
     parser.add_argument("--fp16", action = "store_true", help = "Use fp16")
     parser.add_argument("--seed", type = int, default = None, help = "Seed value")
     parser.add_argument("--iters", type = int, default = 5000, help = "Number of iterations")
@@ -14,13 +16,15 @@ def main():
     parser.add_argument("--maxSteps", type = int, default = 512, help = "Max steps")
     parser.add_argument("--h", type = int, default = 64, help = "Height")
     parser.add_argument("--w", type = int, default = 64, help = "Width")
-    parser.add_argument("--datasetSizeTrain", type = int, default = 20, help = "Dataset size for training")
+    parser.add_argument("--datasetSizeTrain", type = int, default = 100, help = "Dataset size for training")
     parser.add_argument("--datasetSizeValid", type = int, default = 8, help = "Dataset size for validation")
     parser.add_argument("--datasetSizeTest", type = int, default = 100, help = "Dataset size for testing")
     parsedArgs = parser.parse_args()
     args = Args(
         posPrompt = parsedArgs.posPrompt,
         workspace = parsedArgs.workspace,
+        sdVersion = parsedArgs.sdVersion,
+        hfModelKey = parsedArgs.hfModelKey,
         fp16 = parsedArgs.fp16,
         seed = parsedArgs.seed, # Default None
         iters = parsedArgs.iters,
