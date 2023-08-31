22416b6074292b40863f6d3f633dc174105ae374
imyaash <yashppanchal1997@gmail.com>
2023-08-31 10:24:18 +0100

    adding test scripts

---
+++ tests5.bat
@@ -0,0 +1,17 @@
+:: tiger 1e-3
+python cli.py "bengal tiger, full-body" "tiger1e3" --fp16 --seed 0 --iters 5000 --lr 1e-3 --datasetSizeTrain 100 --sdVersion "1.5"
+
+:: tiger 7.75e-4
+python cli.py "bengal tiger, full-body" "tiger775e4" --fp16 --seed 0 --iters 5000 --lr 7.75e-4 --datasetSizeTrain 100 --sdVersion "1.5"
+
+:: octopus 1e-3
+python cli.py "octopus" "octopus1e3" --fp16 --seed 0 --iters 5000 --lr 1e-3 --datasetSizeTrain 100 --sdVersion "1.5"
+
+:: octopus 7.75e-4
+python cli.py "octopus" "octopus775e4" --fp16 --seed 0 --iters 5000 --lr 7.75e-4 --datasetSizeTrain 100 --sdVersion "1.5"
+
+:: tajMahal 1e-3
+python cli.py "taj mahal, full" "tahMahal1e3" --fp16 --seed 0 --iters 5000 --lr 1e-3 --datasetSizeTrain 100 --sdVersion "1.5"
+
+:: tajMahal 7.75e-4
+python cli.py "taj mahal, full" "tajMahal775e4" --fp16 --seed 0 --iters 5000 --lr 7.75e-4 --datasetSizeTrain 100 --sdVersion "1.5"
