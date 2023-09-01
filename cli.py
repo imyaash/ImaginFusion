975032e1f831d4bf6e57199663da45875147af09
imyaash <yashppanchal1997@gmail.com>
2023-09-01 18:08:51 +0100

    Adding docstring and comments to the UI files.

--- cli.py
+++ cli.py
@@ -3,6 +3,17 @@ from utils.args import Args
 from NeRF.pipe import Pipeline
 
 def main():
+    """
+    Main function to run the ImaginFusionCLI.
+
+    This function parses command line arguments, creates an Args object, and initiates & runs the pipeline.
+
+    Args:
+        None
+    
+    Returns:
+        None
+    """
     parser = argparse.ArgumentParser(description = "ImaginFusionCLI")
     parser.add_argument("posPrompt", type = str, help = "Positive prompt for ImaginFusion")
     parser.add_argument("workspace", type = str, help = "Workspace for saving results")
@@ -11,7 +22,7 @@ def main():
     parser.add_argument("--fp16", action = "store_true", help = "Use fp16")
     parser.add_argument("--seed", type = int, default = None, help = "Seed value")
     parser.add_argument("--iters", type = int, default = 5000, help = "Number of iterations")
-    parser.add_argument("--lr", type = float, default = 7.75e-4, help = "Learning rate")
+    parser.add_argument("--lr", type = float, default = 1e-3, help = "Learning rate")
     parser.add_argument("--lambdaEntropy", type = float, default = 1e-4, help = "Lambda entropy")
     parser.add_argument("--maxSteps", type = int, default = 512, help = "Max steps")
     parser.add_argument("--h", type = int, default = 64, help = "Height")
@@ -20,20 +31,17 @@ def main():
     parser.add_argument("--datasetSizeValid", type = int, default = 8, help = "Dataset size for validation")
     parser.add_argument("--datasetSizeTest", type = int, default = 100, help = "Dataset size for testing")
     parsedArgs = parser.parse_args()
+
+    # Creates an Args object to store the parsed arguments
     args = Args(
         posPrompt = parsedArgs.posPrompt,
         workspace = parsedArgs.workspace,
         sdVersion = parsedArgs.sdVersion,
         hfModelKey = parsedArgs.hfModelKey,
         fp16 = parsedArgs.fp16,
-        seed = parsedArgs.seed, # Default None
+        seed = parsedArgs.seed,
         iters = parsedArgs.iters,
         lr = parsedArgs.lr,
-        # lr = 7.75e-4, # trying for speeds sake, good result on simple shaped object
-        # lr = 1e-4, # for slower but better performance, useless for complex and intricate objects takes too long to learn
-        # lr = 1e-3, # is the original
-        # lr = 5.5e-4, # seems to be a good middle ground
-        # lr = 7.75e-5,
         lambdaEntropy = parsedArgs.lambdaEntropy,
         maxSteps = parsedArgs.maxSteps,
         h = parsedArgs.h,
@@ -41,9 +49,15 @@ def main():
         datasetSizeTrain = parsedArgs.datasetSizeTrain,
         datasetSizeValid = parsedArgs.datasetSizeValid,
         datasetSizeTest = parsedArgs.datasetSizeTest
-        # try 0.015 jitterCentre
     )
+
+    # Initialising and running the pipeline with the Args object
     Pipeline(args)()
 
 if __name__ == "__main__":
     main()
+
+# lr = 7.75e-4, # trying for speeds sake, good result on simple shaped object
+# lr = 1e-4, # for slower but better performance, useless for complex and intricate objects takes too long to learn
+# lr = 1e-3, # is the original
+# lr = 5.5e-4, # seems to be a good middle ground
--- gui.py
+++ gui.py
@@ -1,7 +1,6 @@
+import gradio as gr
 from utils.args import Args
 from NeRF.pipe import Pipeline
-
-import gradio as gr
 
 def main(
     posPrompt,
@@ -20,6 +19,29 @@ def main(
     datasetSizeValid,
     datasetSizeTest
 ):
+    """
+    Main function to running the ImaginFusion Application.
+
+    Args:
+        posPrompt (str): Positive prompt for ImaginFusion.
+        workspace (str): Workspace name for saving results.
+        sdVersion (str): Stable Diffusion version
+        hfModelKey (str): HuggingFace model key for Stable Diffusion
+        fp16 (bool): Use mixed precision for training.
+        seed (int): Seed value for reproducibility.
+        iters (int): Number of iterations.
+        lr (float): Learning Rate.
+        lambdaEntropy (float): Loss scale foe alpha entropy.
+        maxSteps (int): Maximum number of steps sampled per ray.
+        h (int): Render height for NeRF training.
+        w (int): Render width for NeRF training.
+        datasetSizeTrain (int): Size of training dataset.
+        datasetSizeValid (int): Size of validation dataset.
+        datasetSizeTest (int): Size of test dataset.
+
+    Returns:
+        str: Path to generated 3D model.
+    """
     args = Args(
         posPrompt = posPrompt,
         workspace = workspace,
@@ -38,93 +60,117 @@ def main(
         datasetSizeTest = int(datasetSizeTest),
     )
 
+    # Initialising & running the pipeline with Args object
     Pipeline(args)()
 
+    # Return the path to the generated 3D model
     return f'outputs/{workspace}/mesh/Mesh.obj'
 
+# Creating an instance of Gradio Interface
 app = gr.Interface(
     fn = main,
+    # Defining the input components for the GUI
     inputs = [
+        # Textbox for Positive Prompt
         gr.Textbox(
             label = "Positive Prompt"
         ),
+        # Textbox for Workspace
         gr.Textbox(
             label = "Workspace",
             info = "Name of the workspace (output folder)"
         ),
+        # Dropdown for selecting Stable Diffusion version. Defaults to "1.5"
         gr.Dropdown(
             label="Stable Diffusion Version",
             choices=["1.4", "1.5", "2.1", "2.1-base"],
-            value = '1.5'
+            value = "1.5"
         ),
+        # Textbox for HuggingFace model key for Stable Diffusion
         gr.Textbox(
             label = "Stable Diffusion HF Model Key"
         ),
+        # Checkbox for enabling Mixed Precision
         gr.Checkbox(
             label = "Mixed Precision",
-            value= True,
+            value= True, # Initially checked
             info = "Using mixed precision to reduce training time and precision"
         ),
+        # Textbox for seed value
         gr.Textbox(
             label = "Seed",
             value = None,
             info = "Seed Value for Reproducibility"
         ),
+        # Number box for number of iteration
         gr.Number(
             label = "Number of iterations",
-            value = 5000
+            value = 5000 # Default value
         ),
+        # Dropdown for Learning rate
         gr.Dropdown(
             label = "Learning Rate",
             choices = ["1e-3", "7.75e-4", "5.5e-4", "1e-4"],
-            value = '1e-3',
+            value = '1e-3', # Default choice
             info = "1e-4 works better with higher iterations, 7.75e-4 & 5.5e-4 should work better for more complex shapes."
         ),
+        # Textbox for Lambda Entropy
         gr.Textbox(
             label = "Lambda Entropy",
-            value = "1e-4",
+            value = "1e-4", # Default value
             info = "Loss Scale for Alpha Entropy"
         ),
+        # Number box for Max Steps
         gr.Number(
             label = "Maximum Steps",
-            value = 512,
+            value = 512, # Default value
             info = "Maximum No. of Steps Sampled per Ray"
         ),
+        # Number box for Training height
         gr.Number(
             label = "Training Height",
-            value = 64,
+            value = 64, # Default value
             info = "Render Height for NeRF Training"
         ),
+        # Number box for Training height
         gr.Number(
             label = "Training Width",
-            value = 64,
+            value = 64, # Default value
             info = "Render Width for NeRF Training"
         ),
+        # Number box for training dataset file
         gr.Number(
             label = "Training Size",
-            value = 100,
+            value = 100, # Default value
             info = "Size of the Tarining Dataset"
         ),
+        # Number box for validation dataset file
         gr.Number(
             label = "Validation Size",
-            value = 8,
+            value = 8, # Default value
             info = "Size of the Validation Dataset"
         ),
+        # Number box for test dataset file
         gr.Number(
             label = "Test Size",
-            value = 100,
+            value = 100, # Default value
             info = "Size of Test Dataset"
         )
     ],
+    # Defining output block
     outputs = gr.Model3D(
         label = "3D Model"
     ),
+    # Setting theme for interface
     theme = gr.themes.Soft(),
+    # Defining the title, description, & article for the interface
     title = "ImaginFusion",
     description = "Text-To-3D Model",
     article = "A Text-To-3D Model Implementation based on Dreamfusion paper, with Stable-Diffusion as Image Generator and TinyCudaNN based InstantNGP NeRF.",
+    # Setting flagging of content to "never"
     allow_flagging = "never"
 )
 
+# Launching the Gradio interface when the script is run.
 if __name__ == "__main__":
     app.launch()
