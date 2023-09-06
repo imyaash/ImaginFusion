934209b5ff5c6bc0fd4d629eaf22584f45509dd3
imyaash <yashppanchal1997@gmail.com>
2023-09-06 12:12:58 +0100

    compiled all test scripts to one

--- testScript.bat
+++ testScript.bat
@@ -1,4 +1,92 @@
 :: Coke bottle (takes about 90 minutes)
 python cli.py "classic coca cola bottle" "Coke2" --fp16 --seed 0 --iters 10000 --lr 7.75e-4 --datasetSizeTrain 50 --sdVersion "1.5"
+
 :: Coke bottle (takes about 30 minutes)
 python cli.py "classic coca cola bottle" "Coke" --fp16 --seed 0 --iters 5000 --lr 7.75e-4 --datasetSizeTrain 100 --sdVersion "1.5"
+
+:: Astronaut 1e-3
+python cli.py "an astronaut, full body" "astronaut1e3" --fp16 --seed 0 --iters 5000 --lr 1e-3 --datasetSizeTrain 100 --sdVersion "1.5"
+
+:: Astronaut 7.75e-4
+python cli.py "an astronaut, full body" "astronaut775e4" --fp16 --seed 0 --iters 5000 --lr 7.75e-4 --datasetSizeTrain 100 --sdVersion "1.5"
+
+:: squirrel-octopus hybrid 1e-3
+python cli.py "a sqirrel-octupus hybrid" "sqirrel-octupusHybrid1e3" --fp16 --seed 0 --iters 5000 --lr 1e-3 --datasetSizeTrain 100 --sdVersion "1.5"
+
+:: squirrel-octopus hybrid 7.75e-4
+python cli.py "a sqirrel-octupus hybrid" "sqirrel-octupusHybrid775e4" --fp16 --seed 0 --iters 5000 --lr 7.75e-4 --datasetSizeTrain 100 --sdVersion "1.5"
+
+:: bunny on pancake 1e-3
+python cli.py "a baby bunny sitting on top of a stack of pancakes" "bunnyPancake1e3" --fp16 --seed 0 --iters 5000 --lr 1e-3 --datasetSizeTrain 100 --sdVersion "1.5"
+
+:: bunny on pancake 7.75e-4
+python cli.py "a baby bunny sitting on top of a stack of pancakes" "bunnyPancake775e4" --fp16 --seed 0 --iters 5000 --lr 7.75e-4 --datasetSizeTrain 100 --sdVersion "1.5"
+
+:: metal bunny on cookies 1e-3
+python cli.py "a metal bunny sitting on top of a stack of chocolate cookies" "metalBunnyCookies1e3" --fp16 --seed 0 --iters 5000 --lr 1e-3 --datasetSizeTrain 100 --sdVersion "1.5"
+
+:: metal bunny on cookies 7.75e-4
+python cli.py "a metal bunny sitting on top of a stack of chocolate cookies" "metalBunnyCookies775e4" --fp16 --seed 0 --iters 5000 --lr 7.75e-4 --datasetSizeTrain 100 --sdVersion "1.5"
+
+:: wolf dog 1e-3
+python cli.py "a dark grey wofl-dog with red highlights" "wolfdog" --fp16 --seed 0 --iters 5000 --lr 1e-3 --datasetSizeTrain 100 --sdVersion "1.5"
+
+:: jaguar 1e-3
+python cli.py "a jaguar, full body" "jaguar" --fp16 --seed 0 --iters 5000 --lr 1e-3 --datasetSizeTrain 100 --sdVersion "1.5"
+
+:: horse 7.75e-4
+python cli.py "a horse galloping, full body" "horse" --fp16 --seed 0 --iters 5000 --lr 7.75e-4 --datasetSizeTrain 100 --sdVersion "1.5"
+
+:: dragon 1e-3
+python cli.py "a dragon blasting flame, full body" "dragon" --fp16 --seed 0 --iters 5000 --lr 1e-3 --datasetSizeTrain 100 --sdVersion "1.5"
+
+:: dragonGOT 1e-3
+python cli.py "a dragon form game of thrones, full body" "dragonGOT1e3" --fp16 --seed 0 --iters 5000 --lr 1e-3 --datasetSizeTrain 100 --sdVersion "1.5"
+
+:: dragonGOT 7.75e-4
+python cli.py "a dragon form game of thrones, full body" "dragonGOT775e4" --fp16 --seed 0 --iters 5000 --lr 7.75e-4 --datasetSizeTrain 100 --sdVersion "1.5"
+
+:: Charizard 1e-3
+python cli.py "charizard from pokemon anime, full body" "Charizard1e3" --fp16 --seed 0 --iters 5000 --lr 1e-3 --datasetSizeTrain 100 --sdVersion "1.5"
+
+:: Charizard 7.75e-4
+python cli.py "charizard from pokemon anime, full body" "Charizard775e4" --fp16 --seed 0 --iters 5000 --lr 7.75e-4 --datasetSizeTrain 100 --sdVersion "1.5"
+
+:: stonehenge 1e-3
+python cli.py "stonehenge" "stonehenge1e3" --fp16 --seed 0 --iters 5000 --lr 1e-3 --datasetSizeTrain 100 --sdVersion "1.5"
+
+:: stonehenge 7.75e-4
+python cli.py "stonehenge" "stonehenge775e4" --fp16 --seed 0 --iters 5000 --lr 7.75e-4 --datasetSizeTrain 100 --sdVersion "1.5"
+
+:: tiger 1e-3
+python cli.py "bengal tiger, full-body" "tiger1e3" --fp16 --seed 0 --iters 5000 --lr 1e-3 --datasetSizeTrain 100 --sdVersion "1.5"
+
+:: tiger 7.75e-4
+python cli.py "bengal tiger, full-body" "tiger775e4" --fp16 --seed 0 --iters 5000 --lr 7.75e-4 --datasetSizeTrain 100 --sdVersion "1.5"
+
+:: octopus 1e-3
+python cli.py "octopus" "octopus1e3" --fp16 --seed 0 --iters 5000 --lr 1e-3 --datasetSizeTrain 100 --sdVersion "1.5"
+
+:: octopus 7.75e-4
+python cli.py "octopus" "octopus775e4" --fp16 --seed 0 --iters 5000 --lr 7.75e-4 --datasetSizeTrain 100 --sdVersion "1.5"
+
+:: tajMahal 1e-3
+python cli.py "taj mahal, full" "tahMahal1e3" --fp16 --seed 0 --iters 5000 --lr 1e-3 --datasetSizeTrain 100 --sdVersion "1.5"
+
+:: tajMahal 7.75e-4
+python cli.py "taj mahal, full" "tajMahal775e4" --fp16 --seed 0 --iters 5000 --lr 7.75e-4 --datasetSizeTrain 100 --sdVersion "1.5"
+
+:: wineflute14
+python cli.py "wine flute" "wineflute14" --fp16 --seed 0 --iters 5000 --lr 1e-3 --datasetSizeTrain 100 --sdVersion "1.4"
+
+:: wineflute15
+python cli.py "wine flute" "wineflute15" --fp16 --seed 0 --iters 5000 --lr 1e-3 --datasetSizeTrain 100 --sdVersion "1.5"
+
+:: wineflute21
+python cli.py "wine flute" "wineflute21" --fp16 --seed 0 --iters 5000 --lr 1e-3 --datasetSizeTrain 100 --sdVersion "2.1"
+
+:: wineflute21-base
+python cli.py "wine flute" "wineflute21-base" --fp16 --seed 0 --iters 5000 --lr 1e-3 --datasetSizeTrain 100 --sdVersion "2.1-base"
+
+:: screwdriver
+python cli.py "screwdriver" "screwdriver" --fp16 --seed 0 --iters 5000 --lr 1e-3 --datasetSizeTrain 100 --sdVersion "1.5"
--- testScript7.bat
+++
@@ -1,2 +0,0 @@
-:: screwdriver
-python cli.py "screwdriver" "screwdriver" --fp16 --seed 0 --iters 5000 --lr 1e-3 --datasetSizeTrain 100 --sdVersion "1.5"
