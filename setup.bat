a5e437fb1894315d066ee4c7eb304f7b0735cd66
imyaash <yashppanchal1997@gmail.com>
2023-09-30 10:07:36 +0100

    updated dir structure

--- setup.bat
+++ setup.bat
@@ -3,5 +3,5 @@ pip install torch torchvision torchaudio --index-url https://download.pytorch.or
 pip install -r requirements.txt
 pip install git+https://github.com/NVlabs/tiny-cuda-nn/#subdirectory=bindings/torch
 pip install git+https://github.com/NVlabs/nvdiffrast/
-pip install ./freqencoder
-pip install ./raymarching
+pip install ./modules/freqencoder
+pip install ./modules/raymarching
